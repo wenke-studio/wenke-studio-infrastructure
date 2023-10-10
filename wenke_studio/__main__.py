cc5e9cd64e7cb0e689aa47e86381f4d6a3501b92
wenke.lai <wenke.lai@wenke-studio.com>
2023-10-10 23:34:10 +0800

    style(wenke_studio): update linting only

--- wenke_studio/__main__.py
+++ wenke_studio/__main__.py
@@ -2,34 +2,40 @@ import pulumi
 import pulumi_aws as aws
 
 
+class IAM:
+    def __init__(self) -> None:
+        owner = aws.iam.Group(
+            "owner",
+            name="owner",
+            opts=pulumi.ResourceOptions(protect=True),
+        )
+
+        human = aws.iam.Group(
+            "human",
+            name="human",
+            opts=pulumi.ResourceOptions(protect=True),
+        )
+
+        wenke_lai = aws.iam.User(
+            "wenke_lai",
+            name="wenke.lai",
+            opts=pulumi.ResourceOptions(protect=True),
+        )
+
+        aws.iam.UserGroupMembership(
+            "wenke_lai",
+            user=wenke_lai.name,
+            groups=[human.name, owner.name],
+            opts=pulumi.ResourceOptions(protect=True),
+        )
+
+
 def main():
-    root = aws.s3.BucketV2(
+    IAM()
+
+    aws.s3.BucketV2(
         "root",
         bucket="wenke-studio-infrastructure",
-        opts=pulumi.ResourceOptions(protect=True),
-    )
-
-    owner = aws.iam.Group(
-        "owner",
-        name="owner",
-        opts=pulumi.ResourceOptions(protect=True),
-    )
-    human = aws.iam.Group(
-        "human",
-        name="human",
-        opts=pulumi.ResourceOptions(protect=True),
-    )
-
-    wenke_lai = aws.iam.User(
-        "wenke_lai",
-        name="wenke.lai",
-        opts=pulumi.ResourceOptions(protect=True),
-    )
-
-    wenke_lai = aws.iam.UserGroupMembership(
-        "wenke_lai",
-        user="wenke.lai",
-        groups=["human", "owner"],
         opts=pulumi.ResourceOptions(protect=True),
     )
 
@@ -39,6 +45,8 @@ def main():
         name="wenke-studio.com",
         opts=pulumi.ResourceOptions(protect=True),
     )
+
+    pulumi.export("zone", wenke_studio.id)
 
 
 if __name__ == "__main__":
