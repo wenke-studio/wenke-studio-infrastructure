4903ad151435cad4a61c8ed239ad28f70835f0e7
wenke.lai <wenke.lai@wenke-studio.com>
2023-10-10 22:52:53 +0800

    feat: import the root resource: s3.bucket, iam.group and iam.user

--- wenke_studio/__main__.py
+++ wenke_studio/__main__.py
@@ -1,5 +1,40 @@
+import pulumi
+import pulumi_aws as aws
+
+
 def main():
-    pass
+    root = aws.s3.BucketV2(
+        "root",
+        bucket="wenke-studio-infrastructure",
+        opts=pulumi.ResourceOptions(protect=True),
+    )
+
+    owner = aws.iam.Group(
+        "owner",
+        name="owner",
+        opts=pulumi.ResourceOptions(protect=True),
+    )
+    human = aws.iam.Group(
+        "human",
+        name="human",
+        opts=pulumi.ResourceOptions(protect=True),
+    )
+
+    wenke_lai = aws.iam.User(
+        "wenke_lai",
+        name="wenke.lai",
+        # tags={
+        #     "AKIAWGRE5SVJUH2XP6WB": "wenke.pc",
+        # },
+        opts=pulumi.ResourceOptions(protect=True),
+    )
+
+    wenke_lai = aws.iam.UserGroupMembership(
+        "wenke_lai",
+        user="wenke.lai",
+        groups=["human", "owner"],
+        opts=pulumi.ResourceOptions(protect=True),
+    )
 
 
 if __name__ == "__main__":
