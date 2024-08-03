9e9cc1251ccdaca8b3f436fe5682326d863dd9eb
wenke.lai <wenke.studio@gmail.com>
2024-08-04 02:00:35 +0800

    fix(user): add the cognito client with Google identity provider

--- user_management/__main__.py
+++ user_management/__main__.py
@@ -29,11 +29,49 @@ def main():
         account_recovery_setting=cognito.UserPoolAccountRecoverySettingArgs(
             recovery_mechanisms=[
                 cognito.UserPoolAccountRecoverySettingRecoveryMechanismArgs(
-                    name="verified_email",  # can be of the following: verified_email, verified_phone_number and admin_only
+                    # can be of the following: verified_email, verified_phone_number and admin_only
+                    name="verified_email",
                     priority=1,
                 )
             ]
         ),
+    )
+
+    google_identity_provider = cognito.IdentityProvider(
+        _("user-management"),
+        user_pool_id=user_pool.id,
+        provider_name="Google",
+        provider_type="Google",
+        provider_details={
+            "authorize_scopes": "email",
+            "client_id": "your client_id",
+            "client_secret": "your client_secret",
+        },
+        attribute_mapping={
+            "email": "email",
+            "username": "sub",
+        },
+    )
+    user_pool_client = cognito.UserPoolClient(
+        _("user-management"),
+        user_pool_id=user_pool.id,
+        generate_secret=False,
+        callback_urls=[
+            "https://example.com/callback",
+        ],
+        logout_urls=[
+            "https://example.com/logout",
+        ],
+        allowed_oauth_flows=[
+            "code",
+            "implicit",
+        ],
+        allowed_oauth_scopes=[
+            "email",
+            "openid",
+        ],
+        # options: COGNITO | SAML | Facebook | Google | LoginWithAmazon | SignInWithApple | OIDC
+        supported_identity_providers=["COGNITO", "Google"],
     )
 
 
