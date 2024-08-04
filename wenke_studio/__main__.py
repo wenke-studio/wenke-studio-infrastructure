8d45d4c97498d2249da7647a020ceeeb3aa9362d
wenke.lai <wenke.studio@gmail.com>
2024-08-04 19:24:02 +0800

    fix(wenke-studio): create an A record for cognito custom domain

--- wenke_studio/__main__.py
+++ wenke_studio/__main__.py
@@ -30,6 +30,70 @@ class IAM:
         )
 
 
+def create_static_website_for_a_record(zone: aws.route53.Zone):
+    resource_name = "root-record"
+
+    bucket = aws.s3.BucketV2(
+        resource_name,
+        # The name of the bucket is the same as the name of the record that you're creating
+        bucket="wenke-studio.com",
+    )
+    aws.s3.BucketObjectv2(
+        resource_name,
+        bucket=bucket.id,
+        key="index.html",
+        source=pulumi.FileAsset("index.html"),
+    )
+    website = aws.s3.BucketWebsiteConfigurationV2(
+        resource_name,
+        bucket=bucket.id,
+        index_document={
+            "suffix": "index.html",
+        },
+    )
+
+    aws.s3.BucketPublicAccessBlock(
+        resource_name,
+        bucket=bucket.id,
+        block_public_acls=False,
+        block_public_policy=False,
+        ignore_public_acls=False,
+        restrict_public_buckets=False,
+    )
+    aws.s3.BucketPolicy(
+        "bucket-policy",
+        bucket=bucket.id,
+        policy=bucket.arn.apply(
+            lambda arn: {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "PublicReadGetObject",
+                        "Effect": "Allow",
+                        "Principal": "*",
+                        "Action": ["s3:GetObject"],
+                        "Resource": [f"{arn}/*"],
+                    }
+                ],
+            }
+        ),
+    )
+
+    aws.route53.Record(
+        resource_name,
+        zone_id=zone.zone_id,
+        type="A",
+        name="wenke-studio.com",  # A record's name is root domain
+        aliases=[
+            {
+                "name": website.website_domain,
+                "zoneId": bucket.hosted_zone_id,
+                "evaluate_target_health": False,
+            }
+        ],
+    )
+
+
 def main():
     IAM()
 
@@ -67,6 +131,8 @@ def main():
         "certificate",
         "arn:aws:acm:us-east-1:426352940371:certificate/383e1e78-5986-46c5-bdcc-951d5fad5207",
     )
+
+    create_static_website_for_a_record(wenke_studio)
 
 
 if __name__ == "__main__":
